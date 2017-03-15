5d16654cad7ef3ccae757cf5a9fe52b25be08aeb
Samuel Durkin <sam.thomas.durkin@gmail.com>
2017-03-15 17:13:13 +0100

    reordering function

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ var zendeskEmail = process.env.zendeskEmail;
 var zendeskToken = process.env.zendeskToken;
 var pipedriveKey = process.env.pipedriveKey;
 
+function zendeskSearch() {
+  zendesk.search.list('query=type:ticket status:new status:open created>2017-01-01').then(function(results){
+      results.forEach(function(result) {
+          console.log("Zendesk Ticket created at " + dateFormat(result.created_at, "dddd, mmmm dS, yyyy, h:MM:ss TT"));
+          console.log(result.subject);
+      });
+  });
+}
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -24,6 +33,7 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  zendeskSearch();
 });
 
 
@@ -41,14 +51,3 @@ var zendesk = new Zendesk({
 //        console.log(notes[i].org_id);
 //    }
 //});
-
-function zendeskSearch() {
-  zendesk.search.list('query=type:ticket status:new status:open created>2017-01-01').then(function(results){
-      results.forEach(function(result) {
-          console.log("Zendesk Ticket created at " + dateFormat(result.created_at, "dddd, mmmm dS, yyyy, h:MM:ss TT"));
-          console.log(result.subject);
-      });
-  });
-}
-
-zendeskSearch();
