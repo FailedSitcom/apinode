6f947b908a0c0a9701170c86adc15332689bc3ec
Samuel Durkin <sam.thomas.durkin@gmail.com>
2017-03-21 16:45:40 +0100

    console logging variables and commenting out all other features

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ var dateFormat = require('dateformat');
 var Pipedrive = require('pipedrive');
 var Zendesk = require('zendesk-node-api');
 
-var zendeskUrl   = process.env['zendeskUrl'];
-var zendeskEmail = process.env['zendeskEmail'];
-var zendeskToken = process.env['zendeskToken'];
-var pipedriveKey = process.env['pipedriveKey'];
+var zendeskUrl   = process.env.ZENDESKURL;
+var zendeskEmail = process.env.ZENDESKEMAIL;
+var zendeskToken = process.env.ZENDESKTOKEN;
+var pipedriveKey = process.env.PIPEDRIVEKEY;
+
+console.log(zendeskUrl);
+console.log(zendeskEmail);
+console.log(zendeskToken);
+console.log(pipedriveKey);
 
 var zendesk = new Zendesk({
     url: zendeskUrl,
@@ -21,65 +26,66 @@ var pipedrive = new Pipedrive.Client(pipedriveKey, {
 });
 
 
-function zendeskSearch() {
-    zendesk.search.list('query=type:ticket status:new status:open created>24hours').then(function(results) {
-        results.forEach(function(result) {
-            pipedrive.Organizations.find({
-                term: result.custom_fields[0].value
-            }, function(err, organization) {
-                if (err) throw err;
-                if (organization.length > 0) {
-                    organization[0].getDeals(function(dealsErr, deals) {
-                        if (dealsErr) throw dealsErr;
-                        if (result.subject.indexOf('[TEST]') == -1) {
-                            if (result.subject.indexOf('New custom question was added') == -1) {
-                                if (result.subject.indexOf('New registration') == -1) {
-                                    pipedrive.Notes.add({
-                                            deal_id: deals[0].id,
-                                            content: "Zendesk Ticket created at " + dateFormat(result.created_at, "dddd, mmmm dS, yyyy, h:MM:ss TT") +
-                                                " ---- " + result.subject
-                                        },
-                                        function(addErr, addData) {
-                                            if (addErr) throw addErr;
-                                            console.log('Note successfully added', addData);
-                                        });
-                                }
-                            }
-                        }
-                    });
-                } else {
-                    pipedrive.Persons.find({
-                        term: result.via.source.from.address
-                    }, function(err, person) {
-                        if (err) throw err;
-                        if (person.length > 0) {
-                            person[0].getDeals(function(dealsErr, deals) {
-                                if (dealsErr) throw dealsErr;
-                                if (result.subject.indexOf('[TEST]') == -1) {
-                                    if (result.subject.indexOf('New custom question was added') == -1) {
-                                        if (result.subject.indexOf('New registration') == -1) {
-                                            pipedrive.Notes.add({
-                                                    deal_id: deals[0].id,
-                                                    content: "Zendesk Ticket created at " + dateFormat(result.created_at, "dddd, mmmm dS, yyyy, h:MM:ss TT") +
-                                                        " ---- " + result.subject
-                                                },
-                                                function(addErr, addData) {
-                                                    if (addErr) throw addErr;
-                                                    console.log('Note successfully added', addData);
-                                                });
-                                        }
-                                    }
-                                }
-                            });
-                        } else {
-                            console.log("undefined");
-                        }
-                    });
-                }
-            });
-        });
-    });
-}
+// function zendeskSearch() {
+//     zendesk.search.list('query=type:ticket status:new status:open created>24hours').then(function(results) {
+//         results.forEach(function(result) {
+//
+//             pipedrive.Organizations.find({
+//                 term: result.custom_fields[0].value
+//             }, function(err, organization) {
+//                 if (err) throw err;
+//                 if (organization.length > 0) {
+//                     organization[0].getDeals(function(dealsErr, deals) {
+//                         if (dealsErr) throw dealsErr;
+//                         if (result.subject.indexOf('[TEST]') == -1) {
+//                             if (result.subject.indexOf('New custom question was added') == -1) {
+//                                 if (result.subject.indexOf('New registration') == -1) {
+//                                     pipedrive.Notes.add({
+//                                             deal_id: deals[0].id,
+//                                             content: "Zendesk Ticket created at " + dateFormat(result.created_at, "dddd, mmmm dS, yyyy, h:MM:ss TT") +
+//                                                 " ---- " + result.subject
+//                                         },
+//                                         function(addErr, addData) {
+//                                             if (addErr) throw addErr;
+//                                             console.log('Note successfully added', addData);
+//                                         });
+//                                 }
+//                             }
+//                         }
+//                     });
+//                 } else {
+//                     pipedrive.Persons.find({
+//                         term: result.via.source.from.address
+//                     }, function(err, person) {
+//                         if (err) throw err;
+//                         if (person.length > 0) {
+//                             person[0].getDeals(function(dealsErr, deals) {
+//                                 if (dealsErr) throw dealsErr;
+//                                 if (result.subject.indexOf('[TEST]') == -1) {
+//                                     if (result.subject.indexOf('New custom question was added') == -1) {
+//                                         if (result.subject.indexOf('New registration') == -1) {
+//                                             pipedrive.Notes.add({
+//                                                     deal_id: deals[0].id,
+//                                                     content: "Zendesk Ticket created at " + dateFormat(result.created_at, "dddd, mmmm dS, yyyy, h:MM:ss TT") +
+//                                                         " ---- " + result.subject
+//                                                 },
+//                                                 function(addErr, addData) {
+//                                                     if (addErr) throw addErr;
+//                                                     console.log('Note successfully added', addData);
+//                                                 });
+//                                         }
+//                                     }
+//                                 }
+//                             });
+//                         } else {
+//                             console.log("undefined");
+//                         }
+//                     });
+//                 }
+//             });
+//         });
+//     });
+// }
 
 app.set('port', (process.env.PORT || 5000));
 
