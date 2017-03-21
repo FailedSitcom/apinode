2ffa3aea4939c09a39dbce5ea36c99d21f7b5e32
Samuel Durkin <sam.thomas.durkin@gmail.com>
2017-03-21 16:11:14 +0100

    adding git ignore file and increasing time query to include catch older tickets

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var dateFormat = require('dateformat');
 var Pipedrive = require('pipedrive');
 var Zendesk = require('zendesk-node-api');
 
-var zendeskUrl   = process.env.zendeskUrl;
-var zendeskEmail = process.env.zendeskEmail;
-var zendeskToken = process.env.zendeskToken;
-var pipedriveKey = process.env.pipedriveKey;
+var zendeskUrl   = process.env['zendeskUrl'];
+var zendeskEmail = process.env['zendeskEmail'];
+var zendeskToken = process.env['zendeskToken'];
+var pipedriveKey = process.env['pipedriveKey'];
 
 var zendesk = new Zendesk({
     url: zendeskUrl,
@@ -22,8 +22,7 @@ var pipedrive = new Pipedrive.Client(pipedriveKey, {
 
 
 function zendeskSearch() {
-
-    zendesk.search.list('query=type:ticket status:new status:open created>2hours').then(function(results) {
+    zendesk.search.list('query=type:ticket status:new status:open created>24hours').then(function(results) {
         results.forEach(function(result) {
             pipedrive.Organizations.find({
                 term: result.custom_fields[0].value
@@ -84,25 +83,7 @@ function zendeskSearch() {
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-    response.render('pages/index');
-});
-
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
     zendeskSearch();
 });
-
-
-// pipedrive.Notes.getAll({}, function(err, notes) {
-//    if (err) throw err;
-//    for (var i = 0; i < notes.length; i++) {
-//        console.log(notes[i].org_id);
-//    }
-// });
